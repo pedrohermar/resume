2b1143db9f4b9a9fb3ca6ff272d980d02de4347a
Pedro <pedrohm92@gmail.com>
2023-01-12 20:21:29 +0100

    Created a resume template

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+const Avatar = () => {
+  return <div className="avatar"></div>;
+};
+
+export default Avatar;
---
+++ src/containers/Resume/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Avatar from "../../components/Avatar";
+import "./styles.scss";
+
+const Resume = () => {
+  return (
+    <div className="resume">
+      <Avatar />
+      <h1>Pedro Hernández Martínez</h1>
+    </div>
+  );
+};
+
+export default Resume;
